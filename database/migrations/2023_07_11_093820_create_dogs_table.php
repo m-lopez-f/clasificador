158cce8f855dfe080db9ccc584c22ae5fa912374
mlopez <mlf.481990@gmail.com>
2023-07-12 01:09:23 +0200

    [DB] Add Models and database migrations

---
+++ database/migrations/2023_07_11_093820_create_dogs_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dogs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('name');
+            $table->text('race');
+            $table->enum('size', ['small', 'medium', 'big', 'extrabig']);
+            $table->text('color');
+            $table->text('path_photo');
+            $table->text('identifier');
+            $table->text('owner');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dogs');
+    }
+}
