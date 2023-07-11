6943fdff66e62185f13c387536f3d08cebdd42af
mlopez <mlf.481990@gmail.com>
2023-07-12 01:11:11 +0200

    [BLADES] Add blades view, first steps of SPA

---
+++ resources/views/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Clasificador</title>
+        <link rel="stylesheet" type="text/css" href="{{asset('dist/app.css')}}">
+    </head>
+    <body>
+        <div class="">
+            <div id="root"></div>
+        </div>
+    </body>
+        <script type="text/javascript" src="{{asset('dist/app.js')}}"></script>
+</html>
